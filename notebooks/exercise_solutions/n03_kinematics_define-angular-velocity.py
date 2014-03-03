122eba4c1548dd394cb3b1b20e0b344721fc0a3a
campanawanna <campanawanna@github.fake>
2014-03-03 12:40:59 -0500

    removed repetative exercises

--- notebooks/exercise_solutions/n03_kinematics_define-angular-velocity.py
+++ notebooks/exercise_solutions/n03_kinematics_define-angular-velocity.py
@@ -1,2 +1,8 @@
+lower_leg_frame.set_ang_vel(inertial_frame, omega1 * inertial_frame.z)
+lower_leg_frame.ang_vel_in(inertial_frame)
+
+upper_leg_frame.set_ang_vel(lower_leg_frame, omega2 * inertial_frame.z)
+upper_leg_frame.ang_vel_in(inertial_frame)
+
 torso_frame.set_ang_vel(upper_leg_frame, omega3 * inertial_frame.z)
 torso_frame.ang_vel_in(inertial_frame)
