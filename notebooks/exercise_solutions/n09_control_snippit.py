f28d0e2f190f1e5e7a472bc805a0847be111c6ad
campanawanna <campanawanna@github.fake>
2014-03-04 16:21:21 -0500

    added exercise solution. hiding output

---
+++ notebooks/exercise_solutions/n09_control_snippit.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+# Controller Design
+
+from numpy import zeros, matrix, eye, dot, asarray
+from numpy.linalg import inv
+from scipy.linalg import solve_continuous_are
+
+## from .utils import controllable
+## from .visualization import *
+
+## equilibrium_point = zeros(len(coordinates + speeds))
+## equilibrium_dict = dict(zip(coordinates + speeds, equilibrium_point))
+parameter_dict = dict(zip(constants, numerical_constants))
+
+linear_state_matrix, linear_input_matrix, inputs = kane.linearize()
+f_A_lin = linear_state_matrix.subs(parameter_dict).subs(equilibrium_dict)
+f_B_lin = linear_input_matrix.subs(parameter_dict).subs(equilibrium_dict)
+m_mat = mass_matrix.subs(parameter_dict).subs(equilibrium_dict)
+
+A = matrix(m_mat.inv() * f_A_lin).astype(float)
+B = matrix(m_mat.inv() * f_B_lin).astype(float)
+
+## assert controllable(A, B)
+
+## Q = matrix(eye(6))
+
+## R = matrix(eye(3))
+
+S = solve_continuous_are(A, B, Q, R)
+
+K = inv(R) * B.T * S
+
+# This is an annoying little issue. We specified the order of things when
+# creating the rhs function, but the linearize function returns the F_B
+# matrix in the order corresponding to whatever order it finds the joint
+# torques. This would also screw things up if we specified a different
+# ordering of the coordinates and speeds as the standard kana._q + kane._u
+
+K = K[[0, 2, 1], :]
+
+
+def controller(x, t):
+    return -asarray(dot(K, x)).flatten()
+
+args['specified'] = controller
+
+y = odeint(right_hand_side, x0, t, args=(args,))
