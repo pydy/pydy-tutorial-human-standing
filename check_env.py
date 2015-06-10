4b9a96ea88ddaf70aea3f8a49f8af4ad0adc7a98
Jason K. Moore <moorepants@gmail.com>
2015-06-10 11:09:42 -0700

    Updated check_env to require PyDy>=0.3.0, IPython>=3.0, SymPy>=0.7.6.

--- check_env.py
+++ check_env.py
@@ -8,13 +8,18 @@ for module in ['IPython', 'sympy', 'pydy', 'numpy', 'scipy', 'matplotlib']:
     except ImportError:
         print('Error: {} not installed'.format(module))
 
+import IPython
 import sympy
 import pydy
 
-if LooseVersion(sympy.__version__) < LooseVersion('0.7.5'):
-    msg = ("Error: SymPy >= 0.7.5 is required for correct notebook "
+if LooseVersion(sympy.__version__) < LooseVersion('0.7.6'):
+    msg = ("Error: SymPy >= 0.7.6 is required for correct notebook "
            + "printing, please upgrade.")
     print(msg)
 
-if LooseVersion(pydy.__version__) < LooseVersion('0.2.1'):
-    print("Error: PyDy >= 0.2.1 is required, please upgrade.")
+if LooseVersion(IPython.__version__) < LooseVersion('3.0.0'):
+    msg = ("Error: IPython >= 3.0.0 is required for the 3D visualizations.")
+    print(msg)
+
+if LooseVersion(pydy.__version__) < LooseVersion('0.3.0'):
+    print("Error: PyDy >= 0.3.0 is required, please upgrade.")
