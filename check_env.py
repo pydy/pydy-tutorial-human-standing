0370988603886a0db7e691a7b9e8134d61af4cff
Jason K. Moore <moorepants@gmail.com>
2015-06-26 00:44:16 -0700

    Made check_env.py work correctly.

--- check_env.py
+++ check_env.py
@@ -8,18 +8,29 @@ for module in ['IPython', 'sympy', 'pydy', 'numpy', 'scipy', 'matplotlib']:
     except ImportError:
         print('Error: {} not installed'.format(module))
 
-import IPython
-import sympy
-import pydy
+try:
+    import sympy
+except ImportError:
+    pass
+else:
+    if LooseVersion(sympy.__version__) < LooseVersion('0.7.6'):
+        msg = ("Error: SymPy >= 0.7.6 is required for correct notebook "
+               + "printing, please upgrade.")
+        print(msg)
 
-if LooseVersion(sympy.__version__) < LooseVersion('0.7.6'):
-    msg = ("Error: SymPy >= 0.7.6 is required for correct notebook "
-           + "printing, please upgrade.")
-    print(msg)
+try:
+    import IPython
+except ImportError:
+    pass
+else:
+    if LooseVersion(IPython.__version__) < LooseVersion('3.0.0'):
+        msg = ("Error: IPython >= 3.0.0 is required for the 3D visualizations.")
+        print(msg)
 
-if LooseVersion(IPython.__version__) < LooseVersion('3.0.0'):
-    msg = ("Error: IPython >= 3.0.0 is required for the 3D visualizations.")
-    print(msg)
-
-if LooseVersion(pydy.__version__) < LooseVersion('0.3.0'):
-    print("Error: PyDy >= 0.3.0 is required, please upgrade.")
+try:
+    import pydy
+except ImportError:
+    pass
+else:
+    if LooseVersion(pydy.__version__) < LooseVersion('0.3.0'):
+        print("Error: PyDy >= 0.3.0 is required, please upgrade.")
